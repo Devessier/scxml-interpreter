be3e48583a7d5dccb79c89709506e43d0df448ea
Devessier <baptiste@devessier.fr>
2021-10-23 17:17:01 +0200

    feat: add link to github repository

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { useAppInterpreterProvider } from "./composables/machine";
 import TheEditor from "./components/TheEditor.vue";
 import TheVisualizer from "./components/TheVisualizer.vue";
+import GithubIcon from "./components/GithubIcon.vue";
 
 useAppInterpreterProvider();
 </script>
@@ -11,5 +12,16 @@ useAppInterpreterProvider();
     <TheEditor />
 
     <TheVisualizer />
+
+    <a
+      href="https://github.com/Devessier/scxml-interpreter"
+      target="_blank"
+      rel="nofollow noopener"
+      class="absolute top-0 right-0 z-10 inline-flex items-center p-1 m-4 text-white bg-gray-800 border border-transparent rounded-md shadow-lg  hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800"
+    >
+      <span class="sr-only">scxml-interpreter on Github</span>
+
+      <GithubIcon />
+    </a>
   </div>
 </template>
